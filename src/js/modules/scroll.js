82d821ba325097d7d7265980159adf8cd4498cdd
ignatjevvv <k.ignatjevvv@gmail.com>
2025-10-25 19:18:44 +0300

    Added scrollup HTML

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -4,21 +4,29 @@ export function scrollActive() {
     const sections = document.querySelectorAll('section[id]');
     const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-      const sectionHeight = current.offsetHeight;
-      const sectionTop = current.offsetTop - 50;
-      let sectionId = current.getAttribute('id');
+    console.log('scrollY: ' + scrollY);
+
+    sections.forEach(section => {
+      const sectionHeight = section.offsetHeight;
+      const sectionTop = section.offsetTop - 56 ;
+      let sectionId = section.getAttribute('id');
+
+      console.log(
+        'Section Name: ' + section.className,
+        'SectionHeight: ' + sectionHeight,
+        'SectionTop: ' + sectionTop,
+      );
 
       if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
         document
           .querySelector(`.nav__menu a[href*=${sectionId}]`)
           .classList.add('active-link');
-        console.log('true:' + sectionId);
+        console.log('true: ' + sectionId);
       } else {
         document
           .querySelector(`.nav__menu a[href*=${sectionId}]`)
           .classList.remove('active-link');
-        console.log(sectionId);
+        console.log('false: ' + sectionId);
       }
     });
   };
